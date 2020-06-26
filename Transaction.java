1ff69950749c3062007ab617d0cbde8583ac8da7
Shruti2906 <patilshruti2906@gmail.com>
2020-06-26 20:43:58 +0530

    Transaction added

---
+++ Transaction.java
@@ -0,0 +1,26 @@
+
+public class Transaction {
+	private int transactionid;
+	private int tamt;
+//	private Date date;
+	public int getTransactionid() {
+		return transactionid;
+	}
+	public void setTransactionid(int transactionid) {
+		this.transactionid = transactionid;
+	}
+	public int getTamt() {
+		return tamt;
+	}
+	public void setTamt(int tamt) {
+		this.tamt = tamt;
+	}
+/*	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}*/
+
+
+}
